b43016300a3824e700da07948f97f3ffacfb28de
Constantin Boubatrin <boubatrin.constantin@gmail.com>
2023-05-23 20:26:19 -0700

    section-5 - JSON with Jackson Fastxml

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -1,20 +1,32 @@
 package com.example.demo.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
     private final Long id;
     private final String name;
+    private final String password;
 
-    Customer(Long id, String name) {
+    Customer(Long id, String name, String password) {
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
+    @JsonProperty("customerId") // change the property name to 'customerId'
     public Long getId() {
         return this.id;
     }
 
+    @JsonProperty("customerName") // change the property name to 'customerName'
     public String getName() {
         return this.name;
+    }
+
+    @JsonIgnore // ignores the property
+    public String getPassword() {
+        return password;
     }
 
     @Override
@@ -22,6 +34,7 @@ public class Customer {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -4,11 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class CustomerRepository implements CustomerRepo {
+
     @Override
     public List<Customer> getCustomers() {
         // TODO connect to real db
         return Collections.singletonList(
-                new Customer(1L, "TODO. Implement real db")
+                new Customer(1L, "TODO. Implement real db", "todo")
         );
     }
 }
