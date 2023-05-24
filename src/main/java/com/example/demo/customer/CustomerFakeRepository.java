0c54591bb08328f8a180d5ec297bfe4048ab40af
Constantin Boubatrin <boubatrin.constantin@gmail.com>
2023-05-23 18:06:21 -0700

    section-4 - Beans

--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -1,11 +1,8 @@
 package com.example.demo.customer;
 
-import org.springframework.stereotype.Component;
-
 import java.util.Arrays;
 import java.util.List;
 
-@Component(value = "fake")
 public class CustomerFakeRepository implements CustomerRepo {
 
     @Override
--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -1,14 +1,8 @@
 package com.example.demo.customer;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-
 import java.util.Collections;
 import java.util.List;
 
-// This class is used to connect to the database
-@Component
-@Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
